46d4a73d1048f6b4882f153be23c93a95711707b
Habib <habib@growrwanda.com>
2025-06-11 14:48:51 +0200

    feat: refactor TodoList to use Slidable for delete action

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({
     super.key,
     required this.taskName,
     required this.taskCompleted,
-    this.onChanged,
+    required this.onChanged,
+    this.deleteFunction,
   });
 
   final String taskName;
   final bool taskCompleted;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 0),
-      child: Container(
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.blueAccent.shade200,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              checkColor: Colors.black,
-              activeColor: Colors.white,
-              side: BorderSide(color: Colors.white, width: 2),
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                decoration:
-                    taskCompleted
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
-                decorationColor: Colors.black,
-                decorationThickness: 2,
-                decorationStyle: TextDecorationStyle.solid,
-              ),
-            ),
+            SlidableAction(onPressed: deleteFunction, icon: Icons.delete),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blueAccent.shade200,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+                side: BorderSide(color: Colors.white, width: 2),
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  decoration:
+                      taskCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                  decorationColor: Colors.black,
+                  decorationThickness: 2,
+                  decorationStyle: TextDecorationStyle.solid,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
